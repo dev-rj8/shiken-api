7a3b8836207a63f0e8780434508004afae4e7da4
dev-rj8 <dev.rjakkani8@gmail.com>
2018-07-16 02:40:24 -0400

    Java Questions added

--- src/main/java/com/devrj/shiken/service/ShikenService.java
+++ src/main/java/com/devrj/shiken/service/ShikenService.java
@@ -1,5 +1,9 @@
 package com.devrj.shiken.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShikenService {
 	
 	@RequestMapping("/questions")
-	public String getQuestions() {
+	public String getQuestions() throws IOException {
 		
-		return "[{id : 1, question : Who is founder of Java?, options : [ Charles babez, Javin Paul, James Gosling]}]";
+		String fileName = "java_questions.json";
+		ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+        
+        //File is found
+        System.out.println("File Found : " + file.exists());
+        
+        //Read File Content
+        String content = new String(Files.readAllBytes(file.toPath()));
+		return content;		
 	}
 	
 }
